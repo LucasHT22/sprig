808f6209535fb88b3c18f4d482611a0b83c8b495
Lucas <88567622+LucasHT22@users.noreply.github.com>
2023-11-02 20:52:55 -0300

    Create all_I_want_for_sprigmas_is_sprig.js

---
+++ games/all_I_want_for_sprigmas_is_sprig.js
@@ -0,0 +1,49 @@
+/*
+I don't want a lot for Sprigmas
+There is just one thing I need
+I don't care about the presents underneath the Sprigmas tree
+I just want sprig for my own
+More than sprig could ever know
+Make my wish come true
+All I want for Sprigmas is sprig
+Yeah
+I don't want a lot for Sprigmas
+There is just one thing I need (and I)
+Don't care about the presents underneath the Sprigmas tree
+I don't need to hang my stocking there upon the fireplace
+Santa Claus won't make me happy with a toy on Sprigmas Day
+I just want sprig for my own
+More than sprig could ever know
+Make my wish come true
+All I want for Sprigmas is sprig
+sprig, baby
+Oh, I won't ask for much this Sprigmas
+I won't even wish for snow (and I)
+I'm just gonna keep on waiting underneath the mistletoe
+I won't make a list and send it to the North Pole for Saint Nick
+I won't even stay awake to hear those magic reindeer click
+'Cause I just want sprig here tonight
+Holding on to me so tight
+What more can I do?
+Oh, baby, all I want for Sprigmas is sprig
+sprig, baby
+Oh-oh, all the lights are shining so brightly everywhere (so brightly, baby)
+And the sound of children's laughter fills the air (oh, oh, yeah)
+And everyone is singing (oh, yeah)
+I hear those sleigh bells ringing
+Santa, won't sprig bring me the one I really need? (Yeah, oh)
+Won't sprig please bring my baby to me?
+Oh, I don't want a lot for Sprigmas
+This is all I'm asking for
+I just wanna see my baby standing right outside my door
+Oh, I just want sprig for my own
+More than sprig could ever know
+Make my wish come true
+Oh, baby, all I want for Sprigmas is sprig
+sprig, baby
+All I want for Sprigmas is sprig, baby
+All I want for Sprigmas is sprig, baby
+All I want for Sprigmas is sprig, baby
+All I want for Sprigmas (all I really want) is sprig, baby
+All I want (I want) for Sprigmas (all I really want) is sprig, baby
+*/
